f91c28f344a69c765b0e4c6e48d6d0c3006096ee
Benishalam <160598480+Benishalam@users.noreply.github.com>
2024-12-09 18:37:48 +0000

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/card";
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <>
+ <HeroSection/>
+ <Card/>
+ </>
+  );
+}
